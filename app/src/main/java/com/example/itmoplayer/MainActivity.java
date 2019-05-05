073014b40ffdd476b1c3c732a88f9bb507957106
Kirill Trezubov <kirilltrezubov@yandex.ru>
2019-05-05 23:05:33 +0300

    "Настройка записи данных пользователя в AccountDetails"
    This reverts commit e54fed4b

--- app/src/main/java/com/example/itmoplayer/MainActivity.java
+++ app/src/main/java/com/example/itmoplayer/MainActivity.java
@@ -1,15 +1,29 @@
 package com.example.itmoplayer;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.lang.Runtime;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int GALLERY_REQUEST = 1;
 
 
     @Override
@@ -23,19 +37,76 @@ public class MainActivity extends AppCompatActivity {
     final EditText password = (EditText) findViewById(R.id.password);
     final EditText email = (EditText) findViewById(R.id.email);
     final EditText error = (EditText) findViewById(R.id.error);
+    final Button image = (Button) findViewById(R.id.chose);
+    final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+            image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, GALLERY_REQUEST);
+            }
+        });
 
         submit.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            if (!(f_name.getText().toString().isEmpty() || s_name.getText().toString().isEmpty()
-                    || login.getText().toString().isEmpty()
-                    || password.getText().toString().isEmpty()
-                    || email.getText().toString().isEmpty())){
+
+            String text_f_name = f_name.getText().toString(),
+                    text_s_name = s_name.getText().toString(),
+                    text_login = login.getText().toString(),
+                    text_password = password.getText().toString(),
+                    text_email = email.getText().toString();
+
+            final String[] user_db_properties = {text_f_name, text_s_name, text_login, text_password,  text_email};
+
+            if (!(text_f_name.isEmpty() || text_s_name.isEmpty()
+                    || text_login.isEmpty()
+                    || text_password.isEmpty()
+                    || text_email.isEmpty() || imageView.getDrawable() == null)){
                 error.setText("");
+
+                Thread thread = new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        try  {
+                            Proxy proxy = new Proxy();
+                            proxy.mainMain("insert_account_details", user_db_properties);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                thread.start();
+
             } else {
                 error.setText("You must put all values in all fields");
             }
         }
     });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        Bitmap bitmap = null;
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+
+        switch(requestCode) {
+            case GALLERY_REQUEST:
+                if(resultCode == RESULT_OK){
+                    Uri selectedImage = imageReturnedIntent.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), selectedImage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    imageView.setImageBitmap(bitmap);
+                }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/itmoplayer/Proxy.java
@@ -0,0 +1,91 @@
+package com.example.itmoplayer;
+
+
+import java.io.IOException;
+import java.util.UUID;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.*;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+
+class Proxy {
+
+    String msg;
+
+    public String mainMain(String DbCommand, String[] user_db_properties) throws InterruptedException, TimeoutException, IOException {
+        EmitNameko client = new EmitNameko();
+        if (DbCommand.equals("insert_account_details")) {
+            String args = String.format("{\"args\": [\"%s\", \"%s\", \"%s\", \"%s\", \"%s\"], \"kwargs\": {}}",
+                    user_db_properties[0], user_db_properties[1], user_db_properties[2], user_db_properties[3], user_db_properties[4]);
+            msg = client.call("account_details.insert_account_details", args);
+        }
+
+
+        return msg;
+    }
+
+
+    public static void main(String[] args)
+            throws IOException, InterruptedException, TimeoutException {
+        EmitNameko client = new EmitNameko();
+        String msg = client.call("greeting_service.hello", "{\"args\": [\"Kirill\", \"Trezubov\", \"Kirill\"], \"kwargs\": {}}");
+    }
+}
+
+class EmitNameko {
+
+    private final String EXCHANGE_NAME = "nameko-rpc";
+    private String replyQueueName ;
+
+    public  String call(String routingKey, String args)
+            throws IOException, InterruptedException, TimeoutException {
+
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("10.0.2.2");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.exchangeDeclare(EXCHANGE_NAME, "topic", true);
+        replyQueueName = channel.queueDeclare().getQueue();
+        channel.queueBind(replyQueueName, EXCHANGE_NAME, replyQueueName);
+
+        final String corrId = UUID.randomUUID().toString();
+
+        AMQP.BasicProperties props = new AMQP.BasicProperties
+                .Builder()
+                .correlationId(corrId)
+                .contentType("application/json")
+                .contentEncoding("utf-8")
+                .replyTo(replyQueueName)
+                .build();
+
+        channel.basicPublish( EXCHANGE_NAME, routingKey, props, args.getBytes("UTF-8"));
+
+        final BlockingQueue<String> response = new ArrayBlockingQueue<String>(1);
+
+        channel.basicConsume(replyQueueName, true, new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                if (properties.getCorrelationId().equals(corrId)) {
+                    String resp = new String(body, "UTF-8");
+                    System.out.println(" [x] Revc '" + resp + "'");
+                    response.offer(resp);
+                }
+            }
+        });
+
+        return response.take();
+
+
+    }
+    //...
+
+
+}
