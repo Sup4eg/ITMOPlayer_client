8f112ce6b89001bd586cab1b36c84c145e5c0aa4
Kirill Trezubov <kirilltrezubov@yandex.ru>
2019-05-30 19:48:34 +0300

    "Первичная настройка фронтенда недавно прослушанной музыки" This reverts commit e54fed4b

---
+++ app/src/main/java/com/example/itmoplayer/RecentlyPlayed.java
@@ -0,0 +1,13 @@
+package com.example.itmoplayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecentlyPlayed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recently_played);
+    }
+}
